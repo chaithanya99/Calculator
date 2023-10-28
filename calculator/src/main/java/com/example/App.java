dbf4fe5f83ca3f0ddf6e0a601a1c88932d270763
chaithanya99 <challapallichaithanya@gmail.com>
2023-10-28 12:48:41 +0530

    added tests and calculator program

--- calculator/src/main/java/com/example/App.java
+++ calculator/src/main/java/com/example/App.java
@@ -1,13 +1,58 @@
 package com.example;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public static int add(int num1,int num2){
+        return num1+num2;
+    }
+    public static int sub(int num1,int num2){
+        return num1-num2;
+    }
+    public static int mul(int num1,int num2){
+        return num1*num2;
+    }
+    public static int div(int num1,int num2){
+        return num1/num2;
+    }
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   
+        int operation=0;
+        while (true) {
+            System.out.println("choose the operations\n 1.Division\n 2.Multiplication\n 3.Addition\n 4.Subtraction\n 5.Exit\n");
+            Scanner S=new Scanner(System.in);
+            operation=S.nextInt();
+            if(operation==5){
+                System.exit(0);
+            }
+
+            System.out.println("Enter 1st number: ");
+            int num1=S.nextInt();
+            System.out.println("Enter 2nd number: ");
+            int num2=S.nextInt();
+
+            if(operation==1){
+                int ans=div(num1,num2);
+                System.out.println("Answer: "+ans);
+            }
+            else if (operation==2) {
+                int ans=mul(num1, num2);
+                System.out.println("Answer: "+ans);
+            } 
+            else if(operation==3) {
+                int ans=add(num1, num2);
+                System.out.println("Answer: "+ans);
+            }
+            else if(operation==4){
+                int ans=sub(num1, num2);
+                System.out.println("Answer: "+ans);
+            }
+
+        }
     }
 }
--- calculator/src/test/java/com/example/AppTest.java
+++ calculator/src/test/java/com/example/AppTest.java
@@ -1,7 +1,7 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,9 +12,60 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    private App cal;
+
+    @Before
+    public void setUP(){
+        cal=new App();
     }
+
+    @Test
+    public void add_positive_numbers(){
+        int a=52;
+        int b=25;
+        int result=77;
+        assertEquals(result, cal.add(a, b));
+    }
+
+    @Test
+    public void add_positive_negative(){
+        int a=-52;
+        int b=25;
+        int result=-27;
+        assertEquals(result, cal.add(a, b));
+    }
+
+    @Test
+    public void add_negative_numbers(){
+        int a=-52;
+        int b=-25;
+        int result=-77;
+        assertEquals(result, cal.add(a, b));
+    }
+
+    @Test
+    public void mul_positive_numbers(){
+        int a=3;
+        int b=7;
+        int result=21;
+        assertEquals(result, cal.mul(a, b));
+    }
+    
+    @Test
+    public void mul_positive_negative(){
+        int a=5;
+        int b=-7;
+        int result=-35;
+        assertEquals(result, cal.mul(a, b));
+    }
+
+    @Test
+    public void mul_negative_numbers(){
+        int a=-5;
+        int b=-20;
+        int result=100;
+        assertEquals(result, cal.mul(a, b));
+    }
+
+   
 }
